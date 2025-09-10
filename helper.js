dbc7f1f3e478867f75e7765f9d7a37b2c61cc901
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-09-10 03:07:14 +0000

    Completed initial display function

--- helper.js
+++ helper.js
@@ -15,4 +15,20 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
 }
 
+function displayLibrary(myLibrary) {
+    const bookCards = document.getElementsByClassName("bookCards");
+    for (books in myLibrary) {
+        const div = document.createElement("div")
+        const title = document.createElement("h1");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
+        const read = document.createElement("p");
+        title.innerText = books.title;
+        author.innerText = books.author;
+        pages.innerText = books.pages;
+        read.innerText = books.read;
+        bookCards.appendChild(div);
+        div.appendChild(title, author, pages, read);
+    }
+}
 
