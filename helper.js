c2ecea8d0ccfd5bf817d7da98837eef338e24de2
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-09-10 03:43:24 +0000

    Working version on display function (1 book)

--- helper.js
+++ helper.js
@@ -1,4 +1,10 @@
 const myLibrary = [];
+const bookCards = document.getElementById("bookCards");
+const div = document.createElement("div");
+const title = document.createElement("h1");
+const author = document.createElement("p");
+const pages = document.createElement("p");
+const read = document.createElement("p");
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -16,19 +22,14 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayLibrary(myLibrary) {
-    const bookCards = document.getElementsByClassName("bookCards");
     for (books in myLibrary) {
-        const div = document.createElement("div")
-        const title = document.createElement("h1");
-        const author = document.createElement("p");
-        const pages = document.createElement("p");
-        const read = document.createElement("p");
-        title.innerText = books.title;
-        author.innerText = books.author;
-        pages.innerText = books.pages;
-        read.innerText = books.read;
+        title.innerText = myLibrary[books].title;
+        author.innerText = myLibrary[books].author;
+        pages.innerText = myLibrary[books].pages;
+        read.innerText = myLibrary[books].read;
         bookCards.appendChild(div);
-        div.appendChild(title, author, pages, read);
+        div.append(title, author, pages, read);
+        console.log(books.title)
     }
 }
 
