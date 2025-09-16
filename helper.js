519486b73f865eafec28b57498d1e740d1a25249
joshp423 <205880399+joshp423@users.noreply.github.com>
2025-09-16 15:23:11 +1200

    Refactored constructors to classes

--- helper.js
+++ helper.js
@@ -2,15 +2,22 @@ const myLibrary = [];
 const bookCards = document.getElementById("bookCards");
 let formActive = false;
 
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
+
+class Book {
+    constructor(title, author, pages, read) {
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor");
+        }
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
     }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+        myLibrary.push(new Book(title, author, pages, read));
 }
 
 bookCards.addEventListener("click", function (event) {
@@ -46,9 +53,6 @@ bookCards.addEventListener("click", function (event) {
     }
 });
 
-function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
-}
 
 function displayLibrary(myLibrary) {
     for (book in myLibrary) {
